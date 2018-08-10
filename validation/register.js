0751c27e0df460302a050fb90b6892255745cff0
Berkay Bindebir <berkaybindebir@gmail.com>
2018-08-10 03:36:06 +0300

    Error Handling

--- validation/register.js
+++ validation/register.js
@@ -21,8 +21,8 @@ module.exports = function validateRegisterInput(data) {
     errors.email = 'Email field is required'
   }
 
-  if (!Validator.isEmpty(data.email)) {
-    errors.email = 'Email is invalid'
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
   }
 
   if (Validator.isEmpty(data.password)) {
