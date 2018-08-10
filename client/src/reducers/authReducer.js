266dccba4595770c5e04ad18d46be196942bce58
Berkay Bindebir <berkaybindebir@gmail.com>
2018-08-10 04:19:47 +0300

    Baby steps on Redux

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+
+  }
+}
