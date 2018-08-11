9841e096c5316d3ae1af3e518425b60d61dc73dd
Berkay Bindebir <berkaybindebir@gmail.com>
2018-08-11 03:21:13 +0300

    Conditional Nav & Logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -11,6 +15,25 @@ import Login from './components/auth/Login';
 
 
 import './App.css';
+
+//Check For Token
+if(localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode Token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set User and isAuth
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if(decoded.exp < currentTime) {
+    // Logout User
+    store.dispatch(logoutUser());
+    // TODO:Clear Current Profile
+    // Redirect to login
+    window.location.href('/login');
+  }
+}
 
 class App extends Component {
   render() {
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 //Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
@@ -13,3 +15,45 @@ export const registerUser = (userData, history) => dispatch => {
       })
     )
 };
+
+// Get token and Login
+export const loginUser = userData => dispatch => {
+  axios.post('/api/users/login', userData)
+    .then(res => {
+      //Save to localStorage
+      const { token } = res.data;
+      // Set token to localStorage
+      // localStorage only stores Strings
+      localStorage.setItem('jwtToken', token);
+      // Set Token to Auth Header
+      setAuthToken(token);
+      // Decode Token
+      const decoded = jwt_decode(token);
+      // Set User
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
+};
+
+// Set Logged User
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
+
+// Log User Out
+export const logoutUser = () => dispatch => {
+  // Remove Token
+  localStorage.removeItem('jwtToken');
+  // Remove auth header for future request
+  setAuthToken(false)
+  // Set Current User to {} which will set isAuth to false
+  dispatch(setCurrentUser({}));
+}
